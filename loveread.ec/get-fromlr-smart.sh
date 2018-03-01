82381eb7684658365d9a5bd873236f497c174d3e
alogic0 <alogic0@gmail.com>
2018-03-01 07:04:53 +0200

    prints num of pages

--- loveread.ec/get-fromlr-smart.sh
+++ loveread.ec/get-fromlr-smart.sh
@@ -11,6 +11,7 @@ function get-body () {
   wget -nv -nc -O $SRC 'http://loveread.ec/read_book.php?id='${IDB}'&p='${PGN}
   iconv -f cp1251 -t utf-8 < $SRC > $FN && echo $FN && rm $SRC
   ../grab-fromlr-2 $FN > $DST && echo "Out: $DST"
+  echo $PGN of $PGUP
 }
 HDR=${BNM}-head.html
 BODY=${BNM}-body.html
@@ -21,6 +22,8 @@ cd "$TMP" || exit 1
 cat /dev/null > $BODY
 get-body 1;
 PGUP=$(../get-numpages ${BNM}-1-utf8.html || exit 1)
+echo Pages: $PGUP
+## echo press any key ...; read zz
 for i in $(seq 2 $PGUP); 
   do get-body $i;
   cat ${BNM}-${i}-pr.html >> $BODY;
@@ -28,5 +31,5 @@ done
 ../grab-fromlr-head ${BNM}-1-utf8.html > ${BNM}-head.html
 cat $HDR $BODY ../foot.html > ${BNM}.html
 rm -v ${BNM}-*-pr.html $HDR $BODY
-echo "Your book is: ${TMP}/${BNM}.html"
+echo "Your book is here: ${TMP}/${BNM}.html"
 cd ..
